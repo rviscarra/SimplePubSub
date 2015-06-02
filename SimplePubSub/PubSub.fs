b6c899e68f18709f040d463793544661ad23166b
Rafael Viscarra <rafaelviscarra@gmail.com>
2015-06-02 16:44:25 -0600

    Generic PubSub 0.1

---
+++ SimplePubSub/PubSub.fs
@@ -0,0 +1,61 @@
+﻿module BoW.Util.PubSub
+
+type PublishCallback<'Msg> = 
+    'Msg -> unit
+
+type internal PubSubCommand<'Id, 'Msg when 'Id : comparison> =
+    | AddSuber of 'Id * PublishCallback<'Msg>
+    | DelSuber of 'Id
+    | Publish  of 'Msg
+    | Stop
+
+type PubSub<'Id, 'Msg when 'Id : comparison> = internal {
+    Actor : MailboxProcessor<PubSubCommand<'Id, 'Msg>>
+} 
+
+type private PubSubState<'Id, 'Msg when 'Id : comparison> = 
+    | PubSubState of Map<'Id, PublishCallback<'Msg>>
+
+let publishToAll msg subMap =
+    subMap
+    |> Map.fold(fun acc _ pubFn ->
+        async {
+            pubFn msg
+        } :: acc
+    ) List.empty
+    |> Async.Parallel
+    |> Async.Ignore
+
+let createPubSub<'Id, 'Msg when 'Id : comparison> () = 
+    let actor = 
+        MailboxProcessor.Start(fun mb ->
+            let rec loop (st : PubSubState<'Id, 'Msg>) =
+                let (PubSubState subMap) = st 
+                async {
+                    let! msg = mb.Receive()
+                    match msg with 
+                    | AddSuber (id, cb) ->
+                        return! loop <| PubSubState (Map.add id cb subMap)
+                    | DelSuber id ->
+                        return! loop <| PubSubState (Map.remove id subMap)
+                    | Publish msg ->
+                        do! publishToAll msg subMap
+                        return! loop <| PubSubState subMap
+                    | Stop ->
+                        return ()
+                }
+            loop <| PubSubState Map.empty<'Id, PublishCallback<'Msg>>
+        )
+    { Actor = actor }
+
+let addSuber id cb ps =
+    ps.Actor.Post <| AddSuber (id ,cb)
+
+let delSuber id ps =
+    ps.Actor.Post <| DelSuber id
+
+let publish msg ps = 
+    ps.Actor.Post <| Publish msg
+
+let stop ps =
+    ps.Actor.Post Stop
---
+++ SimplePubSubTests/Test.fs
@@ -0,0 +1,49 @@
+﻿namespace SimplePubSubTests
+
+open System
+open System.Threading
+open System.Threading.Tasks
+open NUnit.Framework
+
+open BoW.Util
+
+[<TestFixture>]
+type PubSubTest() = 
+
+    [<Test>]
+    member x.``Test PubSub creation``() =
+        let ps = PubSub.createPubSub<unit, unit>() 
+        Assert.IsNotNull ps
+
+    [<Test>]
+    member __.``Test publish without subscribers``() =
+        let ps = PubSub.createPubSub<unit, string>()
+        ps 
+        |> PubSub.publish "Hello World"
+
+    [<Test>]
+    member __.``Test addSuber and publish``() =
+        let msg = "test_message"
+        let ps = PubSub.createPubSub<int, string>()
+        let result = ref "timeout"
+
+        ps |> PubSub.addSuber 0 (fun msg ->
+            result := msg
+        )
+        ps |> PubSub.publish msg
+        Thread.Sleep 100
+        Assert.AreEqual(msg, !result)
+
+    [<Test>]
+    member __.``Test delSuber and publish`` () = 
+        let msg = "test_message"
+        let ps = PubSub.createPubSub<int, string>()
+        let result = ref "timeout"
+
+        ps |> PubSub.addSuber 0 (fun msg ->
+            result := msg
+        )
+        ps |> PubSub.delSuber 0
+        ps |> PubSub.publish msg
+        Thread.Sleep 200
+        Assert.AreNotEqual(msg, !result)
